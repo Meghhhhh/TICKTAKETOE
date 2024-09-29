4b82d1a1bf037c5a4629660b3a62f56e77f32fe1
Shubh Doshi <shubhdoshi21@gmail.com>
2024-09-29 17:52:07 +0530

    proxy

--- server/index.js
+++ server/index.js
@@ -4,11 +4,12 @@ const mongoose = require("mongoose");
 const passport = require("./config/passport.js");
 const error = require("./utils/error.js");
 const session = require("express-session");
-const cron = require('node-cron');
+const cron = require("node-cron");
 const MongoStore = require("connect-mongo");
 const cookieParser = require("cookie-parser");
-const User = require("./models/user.js")
-const { sendEmail } = require("./utils/nodeMailer.js")
+const cors = require("cors"); // Import the cors package
+const User = require("./models/user.js");
+const { sendEmail } = require("./utils/nodeMailer.js");
 const localAuth = require("./routes/localAuth.js");
 const googleAuth = require("./routes/googleAuth.js");
 const users = require("./routes/users.js");
@@ -19,6 +20,15 @@ const port = process.env.PORT || 3000;
 const payment = require("./routes/payment.js");
 const books = require("./routes/books.js");
 const feedback = require("./routes/feedback.js");
+
+// Enable CORS for all origins
+app.use(
+  cors({
+    origin: "*", // Allow all origins
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true, // Allow cookies to be sent from the
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -47,23 +57,24 @@ app.use("/resource/api/v1", resource);
 app.use("/feedback/api/v1", feedback);
 app.use(error);
 
-cron.schedule('0 0 * * *', async () => { 
+cron.schedule("0 0 * * *", async () => {
   try {
     const users = await User.find({ endDate: { $exists: true } });
 
     for (const user of users) {
       const { endDate, email } = user;
-      const oneDayBeforeEndDate = moment(endDate).subtract(1, 'days');
+      const oneDayBeforeEndDate = moment(endDate).subtract(1, "days");
       const currentDate = moment();
-      if (currentDate.isSame(oneDayBeforeEndDate, 'day')) {
-        const subject = 'Reminder: Your End Date is Tomorrow!';
-        const text = 'This is a reminder that your book lend date ends tomorrow.';
+      if (currentDate.isSame(oneDayBeforeEndDate, "day")) {
+        const subject = "Reminder: Your End Date is Tomorrow!";
+        const text =
+          "This is a reminder that your book lend date ends tomorrow.";
 
         await sendEmail(email, subject, text);
       }
     }
   } catch (err) {
-    console.error('Error fetching users or sending emails:', err);
+    console.error("Error fetching users or sending emails:", err);
   }
 });
 
