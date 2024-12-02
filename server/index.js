8cb2b8742af5c012ca76bca47036afb1b6be3c91
Shubh Doshi <shubhdoshi21@gmail.com>
2024-12-02 22:37:47 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -32,6 +32,7 @@ app.use(
     origin: [
       "https://tictechtoe.onrender.com",
       "http://tictechtoe.onrender.com",
+      "http://localhost:5173/"
     ], // Allow all origins
     methods: "GET,POST,PUT,DELETE",
     credentials: true, // Allow cookies to be sent from the
